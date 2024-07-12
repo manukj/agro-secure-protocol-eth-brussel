0f21fc270aa930b55ef89a8efc82b7dc8175d90a
Manu <manu1998kj@gmail.com>
2024-07-12 22:46:04 +0200

    feat: Add login screen component
    The code changes include:
    - Adding a new file `login/page.tsx` which contains the login screen component.
    - Importing the `LoginScreen` component in the `page.tsx` file.
    - Replacing the existing content in the `Home` component with the `LoginScreen` component.
    
    This commit adds the login screen component to the project.

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = getMetadata({
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html suppressHydrationWarning>
+    <html suppressHydrationWarning data-theme="lofi">
       <body>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
---
+++ packages/nextjs/app/screens/login/page.tsx
@@ -0,0 +1,8 @@
+const LoginScreen = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+};
+export default LoginScreen;
